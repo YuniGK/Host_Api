48f8bee354dd73cec60be2a875117c9b55366795
yunkim <1106qwe@hanmail.net>
2024-02-19 21:32:39 +0900

    설정 추가
    Rest Repositories HAL Explorer / Rest Repositories 설정 추가

--- src/main/java/yun/jung/kim/Host_Api/repository/HostRepository.java
+++ src/main/java/yun/jung/kim/Host_Api/repository/HostRepository.java
@@ -1,7 +1,9 @@
 package yun.jung.kim.Host_Api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import yun.jung.kim.Host_Api.domain.Host;
 
+@RepositoryRestResource
 public interface HostRepository extends JpaRepository<Host, Long> {
 }
