6c4d93d4afde433d4e3bbc4ac44733bf67d3a33d
yunkim <1106qwe@hanmail.net>
2024-02-20 15:01:54 +0900

    Query dsl 설정 추가

--- src/main/java/yun/jung/kim/Host_Api/repository/HostRepository.java
+++ src/main/java/yun/jung/kim/Host_Api/repository/HostRepository.java
@@ -1,9 +1,29 @@
 package yun.jung.kim.Host_Api.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import yun.jung.kim.Host_Api.domain.Host;
+import yun.jung.kim.Host_Api.domain.QHost;
 
 @RepositoryRestResource
-public interface HostRepository extends JpaRepository<Host, Long> {
+public interface HostRepository extends JpaRepository<Host, Long>,
+        QuerydslPredicateExecutor<Host>,//검색 기능
+        QuerydslBinderCustomizer<QHost>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QHost root){
+        bindings.excludeUnlistedProperties(true);
+
+        bindings.including(root.name, root.ip, root.createdAt, root.createdBy);
+        bindings.bind(root.ip).first(StringExpression::containsIgnoreCase);//Like %${v}%
+        bindings.bind(root.name).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    };
 }
--- src/main/java/yun/jung/kim/Host_Api/repository/WatchRepository.java
+++ src/main/java/yun/jung/kim/Host_Api/repository/WatchRepository.java
@@ -1,10 +1,31 @@
 package yun.jung.kim.Host_Api.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import yun.jung.kim.Host_Api.domain.Host;
+import yun.jung.kim.Host_Api.domain.QHost;
+import yun.jung.kim.Host_Api.domain.QWatch;
 import yun.jung.kim.Host_Api.domain.Watch;
 
 @RepositoryRestResource
-public interface WatchRepository extends JpaRepository<Watch, Long> {
+public interface WatchRepository extends JpaRepository<Watch, Long>,
+        QuerydslPredicateExecutor<Watch>,//검색 기능
+        QuerydslBinderCustomizer<QWatch>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QWatch root){
+        bindings.excludeUnlistedProperties(true);
+
+        bindings.including(root.eventOccurrence, root.eventResult, root.eventType, root.createdAt, root.createdBy);
+        bindings.bind(root.eventOccurrence).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.eventResult).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.eventType).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    };
 }
